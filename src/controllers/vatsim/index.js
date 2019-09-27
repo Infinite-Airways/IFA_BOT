68954133b3d99ce70c6ea31aca825f9300dbc75b
onskadthecamel <onskadthecamel786@outlook.com>
2019-09-28 00:08:36 +0100

    Start of something new :)

--- src/controllers/vatsim/index.js
+++ src/controllers/vatsim/index.js
@@ -1,6 +1,26 @@
 'use strict';
+const Discord = require('discord.js');
+const fetch = require("node-fetch");
+var text = "";
+
+fetch('http://us.data.vatsim.net/vatsim-data.txt')
+    .then(response => response.text())
+    .then(text => console.log(JSON.parse(text)))
+
+var timerID = setInterval(function() {
+    fetch('http://us.data.vatsim.net/vatsim-data.txt')
+    .then(response => response.text())
+    .then(text => console.log(text))
+}, 120 * 1000); 
+
+
 const vatsimController = message => {
-  message.channel.send('Vatsim!');
+
+    message.channel.send('Vatsim!');
 };
+
+
+
+
 
 module.exports = vatsimController;
