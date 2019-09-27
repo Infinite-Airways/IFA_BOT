b61dfd98b157ae8c650f3df7f39ba49943702abd
onskadthecamel <onskadthecamel786@outlook.com>
2019-09-27 20:46:12 +0100

    Added role on join: Unverified Pilot

--- src/index.js
+++ src/index.js
@@ -32,10 +32,12 @@ bot.on('ready', () => {
 
 bot.on('guildMemberAdd', member => {
   let memberRole = member.guild.roles.find(role => role.id == '483854895615836160');
+  let unverifiedRole = member.guild.roles.find(role => role.id == '566752818564694017');
   const channel = member.guild.channels.find(ch => ch.name === 'welcome');
   if (!channel) return;
   channel.send(`Hey ${member}, welcome to Infinite Airways, enjoy your stay :tada::hugging: !`);
   member.addRole(memberRole);
+  member.addRole(unverifiedRole);
 });
 
 bot.login(token);
