f7d521c4397686b35e73e21b81cad81f88703516
umarf786 <onskadthecamel786@outlook.com>
2020-04-03 03:01:03 +0100

    fixed vatsim stuff with new parsing server - need youness to set that up on his server, the url is already setup port 2223, working locally

--- src/services/vatsim/index.js
+++ src/services/vatsim/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const axios = require('axios');
-const baseUrl = 'http://142.44.243.147:5000/data';
+const baseUrl = 'http://184.161.214.19:2223/data';
 
 const getDataForVatsim = callsign_data => {
   const url = `${baseUrl}/${callsign_data}`;
